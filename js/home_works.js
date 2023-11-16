2bdb5286d851b8c065d155a843d746281470b807
Botagoz Kulzhambekova <mikluhamailru@gmail.com>
2023-11-16 14:36:08 -0600

    2

--- js/home_works.js
+++ js/home_works.js
@@ -1,5 +1,14 @@
 // HOME WORK 1. EMAIL CHECKER
+// 1)  Доработать прошлое ДЗ! Нужно чтобы малый квадрат теперь двигался по всему родительскому 
+// квадрату. Дорабатываете код внутри проекта.
 
+// 2) Отобразить на странице цифру 0 , создать 2 кнопки: stop и start
+// Если нажать на start то цифра начнет увеличиваться на единицу до того момента пока вы 
+// не нажмете на кнопку stop
+// Используйте методы которые мы прошли на уроке. При нажатии на start повторно она должна
+//  продолжить тот счет на котором остановился. Добавить на кнопку reset обнуление счетчика.
+//   Обработать все возможные баги при работе счетчика (вёрстка в проекте уже есть) 
+// Код писать внутри проекта (home_works.js)
 const gmailInput=document.querySelector("#gmail_input");
 const buttonGmail=document.querySelector("#gmail_button");
 const gmailSpan=document.querySelector("#gmail_result");
@@ -19,25 +28,81 @@ buttonGmail.addEventListener('click', () => {
 })
 // HOME WORK 2. 
 
-const parentBlock=document.querySelector("#parent_block");
-// const childBlock=document.querySelector("#child_block");
-// const newParent=document.querySelector("#newParent");
+// const parentBlock=document.querySelector("#parent_block");
+// // const childBlock=document.querySelector("#child_block");
+// // const newParent=document.querySelector("#newParent");
+// // Получаем маленький блок
+// const childBlock=document.getElementById("#movingBlock");
 
+// // Функция для движения маленького блока вправо
+// function moveRight(position) {
+//     if (position < 300) { // Здесь 300 - это максимальная позиция справа, вы можете изменить её на своё усмотрение
+//         position += 1; // Изменение позиции блока
+//         movingBlock.style.left = position + "px"; // Установка новой позиции
+//         setTimeout(function() {
+//             moveRight(position); // Рекурсивный вызов функции для продолжения движения
+//         }, 10); // Интервал времени (10 миллисекунд) между шагами движения
+//     }
+// }
+// // Начнем движение
+// moveRight(0);
+const childBlock=document.querySelector('.child_block')
+let positionX=0;
+let positionY=0;
 
-
-// Получаем маленький блок
-const childBlock=document.getElementById("#movingBlock");
-
-// Функция для движения маленького блока вправо
-function moveRight(position) {
-    if (position < 300) { // Здесь 300 - это максимальная позиция справа, вы можете изменить её на своё усмотрение
-        position += 1; // Изменение позиции блока
-        movingBlock.style.left = position + "px"; // Установка новой позиции
-        setTimeout(function() {
-            moveRight(position); // Рекурсивный вызов функции для продолжения движения
-        }, 10); // Интервал времени (10 миллисекунд) между шагами движения
+const moveChildBlock=() => {
+    if(positionX<448 && positionY===0){
+        // +1 move right
+            positionX++  
+            childBlock.style.left=`${positionX}px`
+            setTimeout(moveChildBlock,10)
+        }else if(positionX>=448 && positionY<448){
+            // move down
+            positionY++
+            childBlock.style.top=`${positionY}px`
+            setTimeout(moveChildBlock,10)
+        }else if(positionX>0 && positionY>=448){
+            // move left
+            positionX--;
+            childBlock.style.left=`${positionX}px`
+            setTimeout(moveChildBlock,10)
+            // move up
+        }else if (positionX===0 && positionY>0){
+            positionY--;
+            childBlock.style.top=`${positionY}px`;
+            setTimeout(moveChildBlock,10);
+        }
     }
-}
-
-// Начнем движение
-moveRight(0);
+moveChildBlock()
+// 1)  Доработать прошлое ДЗ! Нужно чтобы малый квадрат теперь двигался 
+// по всему родительскому квадрату. Дорабатываете код внутри проекта.
+// 2) Отобразить на странице цифру 0 , создать 2 кнопки: stop и start
+// Если нажать на start то цифра начнет увеличиваться на единицу до того момента 
+// пока вы не нажмете на кнопку stop
+// Используйте методы которые мы прошли на уроке. При нажатии на start повторно 
+// она должна продолжить тот счет на котором остановился. Добавить на кнопку reset
+//  обнуление счетчика. Обработать все возможные баги при работе счетчика (вёрстка 
+//     в проекте уже есть) Код писать внутри проекта (home_works.js)
+const startBtn=document.querySelector('#start')
+const stopBtn=document.querySelector('#stop')
+const resetBtn=document.querySelector('#reset')
+const intervalNumber=document.querySelector('.interval')
+let counter =0;
+let intervalId;
+startBtn.addEventListener('click',()=>{
+    console.log(start)
+    intervalId=setInterval(()=>{
+        counter++;
+        intervalNumber.textContent=counter;
+    },1000);
+} );
+stopBtn.addEventListener('click',()=>{
+    console.log("stop")
+    clearInterval(intervalId);
+} )
+resetBtn.addEventListener('click',()=>{
+    console.log(reset);
+    counter=0;
+    intervalNumber.textContent=counter;
+    clearInterval(intervalId);
+} )
---
+++ js/lesson.js
@@ -0,0 +1,12 @@
+const tabsContenCards=document.querySelectorAll('.tab_content_block')
+const tabsItems=document.querySelectorAll('.tab_content_item')
+const tabsItemsParent=document.querySelector('.tab_content_items')
+
+const hideTabsContentCards=()=>{
+    tabsContenCards.forEach(tabsContenCard)=>{
+        tabsContenCard.style.display='none'
+    })
+    tabsItems.forEach((tabItem)=>)
+}
+ const showTabsContentCards=(indexElement)=>
+ 
